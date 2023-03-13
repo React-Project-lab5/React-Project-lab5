29a87846ad91e8861c0efa567d2c2c64b1011dc8
hyebeen byun <hyebeen2658@naver.com>
2023-03-13 17:03:23 +0900

    [style] Chatting component UI구현 #12

--- src/components/ChatDialog/Chatting/Chatting.tsx
+++ src/components/ChatDialog/Chatting/Chatting.tsx
@@ -1,5 +1,24 @@
+import avatar from '@/assets/chatAvatars.svg';
+import message from '@/assets/chatMessage.svg';
 import classes from './Chatting.module.scss';
+import { ChattingInput, Messages } from '../index';
 
 export function Chatting() {
-  return <div className={classes.chatting}></div>;
+  return (
+    <div className={classes.chatting}>
+      <div className={classes.chattingInfo}>
+        <img
+          src="https://avatars.githubusercontent.com/u/38703262?v=4"
+          alt="사용자 프로필"
+        />
+        <span>김서현</span>
+        <div className={classes.chatIcons}>
+          <img src={avatar} alt="마이 페이지로 가기" />
+          <img src={message} alt="메세지 창으로 가기" />
+        </div>
+      </div>
+      <Messages />
+      <ChattingInput />
+    </div>
+  );
 }
