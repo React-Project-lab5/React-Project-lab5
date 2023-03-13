63da2a4144d3b33dd483852e25435eaa2bbe6a39
binwoonki <qlsdnsrl54@gmail.com>
2023-03-13 13:08:14 +0900

    [chore] Modal 팝업창 리팩토링 Props 설정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,16 +1,21 @@
 /* modal.tsx */
 
-import classes from "./Modal.module.scss";
-import close from "@/assets/close.svg";
+import classes from './Modal.module.scss';
+import close from '@/assets/close.svg';
 
-export default function Modal() {
+export default function Modal({ widthValue, heightValue }) {
+  const modalStyle = {
+    width: widthValue,
+    height: heightValue,
+  };
+
   return (
-    <div className={classes["modal-wrap"]}>
-      <div className={classes["modal-top"]}>
+    <div className={classes['modal-wrap']} style={modalStyle}>
+      <div className={classes['modal-top']}>
         <button
           type="button"
           aria-label="닫기버튼"
-          className={classes["close"]}
+          className={classes['close']}
         >
           <img className={classes.img} src={close} alt="닫기 버튼" />
         </button>
