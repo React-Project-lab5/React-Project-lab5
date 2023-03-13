7c6313f63301b649cf7ba0ce50179a1c66116722
hyebeen byun <hyebeen2658@naver.com>
2023-03-13 17:03:50 +0900

    [style] Navbar component UI구현

--- src/components/ChatDialog/Navbar/Navbar.tsx
+++ src/components/ChatDialog/Navbar/Navbar.tsx
@@ -1,5 +1,16 @@
 import classes from './Navbar.module.scss';
-
 export function Navbar() {
-  return <div className={classes.navbar}></div>;
+  return (
+    <div className={classes.navbar}>
+      <p className={classes.logo}>슬기로운 N밥생활</p>
+      <div className={classes.user}>
+        <img
+          src="https://avatars.githubusercontent.com/u/104710243?v=4"
+          alt="사용자"
+        />
+        <p>변혜빈</p>
+        <button type="button">로그아웃</button>
+      </div>
+    </div>
+  );
 }
