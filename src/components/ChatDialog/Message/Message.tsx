92698dfe3da45b413af96e7868b12132c9369a44
hyebeen byun <hyebeen2658@naver.com>
2023-03-13 17:07:44 +0900

    [style] Message component UI구현#17

--- src/components/ChatDialog/Message/Message.tsx
+++ src/components/ChatDialog/Message/Message.tsx
@@ -1,5 +1,22 @@
+import classNames from 'classnames';
 import classes from './Message.module.scss';
-
-export function Message() {
-  return <div className={classes.message}></div>;
-}
+export const Message = () => {
+  return (
+    <div className={classNames(classes.message, classes.owner)}>
+      <div className={classes.messageInfo}>
+        <img
+          src="https://avatars.githubusercontent.com/u/38703262?v=4"
+          alt=""
+        />
+        <span>지금</span>
+      </div>
+      <div className={classes.messageContent}>
+        <p>안녕하세요☺️</p>
+        <img
+          src="https://avatars.githubusercontent.com/u/38703262?v=4"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
