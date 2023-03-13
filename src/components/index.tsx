510847e994248b380a059bbfd5709a0af7be63da
CALKO9611 <kimd9669@naver.com>
2023-03-13 17:54:05 +0900

    [chore] index.tsx에 Button, Input 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,3 +5,5 @@ export * from './Footer/Footer';
 export * from './Nav/Nav';
 export * from './Modal/Modal';
 export * from './Banner/Banner';
+export * from './Button/index';
+export * from './Input/Input';
