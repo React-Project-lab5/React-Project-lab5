da1a44399f91087ef978e2a523be7f962aaba66b
hyebeen byun <hyebeen2658@naver.com>
2023-03-13 17:05:59 +0900

    [style] Search component UI구현 #14

--- src/components/ChatDialog/Search/Search.tsx
+++ src/components/ChatDialog/Search/Search.tsx
@@ -1,5 +1,23 @@
 import classes from './Search.module.scss';
 
-export function Search() {
-  return <div className={classes.search}></div>;
-}
+export const Search = () => {
+  return (
+    <div className={classes.search}>
+      <div className={classes.searchForm}>
+        <label htmlFor="user" className="a11yHidden">
+          사용자 검색
+        </label>
+        <input type="text" placeholder="Find a user" id="user" />
+      </div>
+      <div className={classes.userChat}>
+        <img
+          src="https://avatars.githubusercontent.com/u/38703262?v=4"
+          alt=""
+        />
+        <div className={classes.userChatInfo}>
+          <span>김서현</span>
+        </div>
+      </div>
+    </div>
+  );
+};
