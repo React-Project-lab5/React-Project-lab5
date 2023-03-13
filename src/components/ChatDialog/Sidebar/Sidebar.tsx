0610ed8405b1331ee0bbd87e0143201bdd27198d
hyebeen byun <hyebeen2658@naver.com>
2023-03-13 17:06:57 +0900

    [style] Sidebar component UI구현 #11

--- src/components/ChatDialog/Sidebar/Sidebar.tsx
+++ src/components/ChatDialog/Sidebar/Sidebar.tsx
@@ -1,5 +1,12 @@
+import { Chattings, Navbar, Search } from '../index';
 import classes from './Sidebar.module.scss';
 
 export function Sidebar() {
-  return <div className={classes.sidebar}></div>;
+  return (
+    <div className={classes.sidebar}>
+      <Navbar />
+      <Search />
+      <Chattings />
+    </div>
+  );
 }
