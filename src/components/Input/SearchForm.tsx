b1da94e7447f5a095abbfb5a3c69f38009e38d82
binwoonki <qlsdnsrl54@gmail.com>
2023-03-15 17:55:58 +0900

    [chore] searchForm props 변경 및 리팩토링

--- src/components/Input/SearchForm.tsx
+++ src/components/Input/SearchForm.tsx
@@ -8,18 +8,32 @@ export function SearchFrom() {
     <>
       <div className={classes['InputContainer']}>
         <div className={classes['mainInput']}>
-          <InputSelector widthValue={'200px'} heightValue={'75px'} />
+          <InputSelector
+            widthValue={'200px'}
+            heightValue={'75px'}
+            className={classes.searchFormInputSelector}
+          />
           <Input
             maxWidthValue={'35rem'}
             heightValue={'75px'}
             labelText="검색창"
             isA11yHidden
+            className={classes.searchFormInput}
           />
           <Button
             widthValue={'190px'}
             heightValue={'75px'}
             text="모임 만들기"
             backgroundColor={'orange'}
+            className={classes.MeetingButton}
+          />
+
+          <Button
+            widthValue={'190px'}
+            heightValue={'75px'}
+            text="채팅 하기"
+            backgroundColor={'orange'}
+            className={classes.ChatButton}
           />
         </div>
       </div>
