a03e9cbb2a899d24203ea9b58bc84075ac9a3cdf
binwoonki <qlsdnsrl54@gmail.com>
2023-03-15 13:33:53 +0900

    [feat] Meeting UI 구현 완성

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,51 @@
+import { Meeting } from '@/components/Card';
+import Card from '@/components/Card/Card';
 import { Banner } from '@/components/index';
+import { SearchFrom } from '@/components/Input/SearchForm';
+import classes from './MainPage.module.scss';
 export default function MainPage() {
   return (
     <>
       <Banner />
+      <SearchFrom />
+      <div className={classes['meetingContainer']}>
+        <Card>
+          <Meeting
+            toDate="3월15일"
+            time="12시"
+            town="금천구"
+            place="시흥사거리"
+            title="모집합니다"
+          />
+        </Card>
+        <Card>
+          <Meeting
+            toDate="3월15일"
+            time="12시"
+            town="금천구"
+            place="시흥사거리"
+            title="모집합니다"
+          />
+        </Card>
+        <Card>
+          <Meeting
+            toDate="3월15일"
+            time="12시"
+            town="금천구"
+            place="시흥사거리"
+            title="모집합니다"
+          />
+        </Card>
+        <Card>
+          <Meeting
+            toDate="3월15일"
+            time="12시"
+            town="금천구"
+            place="시흥사거리"
+            title="모집합니다"
+          />
+        </Card>
+      </div>
     </>
   );
 }
