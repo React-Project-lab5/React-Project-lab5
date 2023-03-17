e090c8104313c5aefc21747eea9bee6d6d4b362c
hyebeen byun <hyebeen2658@naver.com>
2023-03-17 17:23:33 +0900

    [chore] button component 수정

--- src/components/Button/ButtonGoogle.tsx
+++ src/components/Button/ButtonGoogle.tsx
@@ -1,33 +1,18 @@
 import classes from './Button.module.scss';
 import google from '/public/assets/googleLogo.svg';
-import { GoogleAuthProvider, signInWithPopup } from '@firebase/auth';
-import { auth } from '@/firebase/auth/index';
-import { useState } from 'react';
+
 interface Props {
   text: '로그인' | '회원가입';
 }
 
 export function ButtonGoogle({ text }: Props) {
-  const [userData, setUserData] = useState(null);
-
-  const handleGoogleLogin = () => {
-    const provider = new GoogleAuthProvider(); // provider를 구글로 설정
-    signInWithPopup(auth, provider) // popup을 이용한 signup
-      .then((data) => {
-        setUserData(data.user); // user data 설정
-        console.log(data); // console로 들어온 데이터 표시
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  const resgi = () => {
-    console.log('클릭');
+  const handleGoogleSignIn = () => {
+    console.log('handleGoogleSignIn');
   };
 
-  // const login = () => {
-  //   console.log('클릭2');
-  // };
+  const handleGoogleSignUp = () => {
+    console.log('handleGoogleSignUp');
+  };
 
   return (
     <>
@@ -35,12 +20,11 @@ export function ButtonGoogle({ text }: Props) {
         type="button"
         aria-label={'Google ' + text + ' 버튼'}
         className={classes.button}
-        onClick={text === '회원가입' ? resgi : handleGoogleLogin}
+        onClick={text === '회원가입' ? handleGoogleSignUp : handleGoogleSignIn}
       >
         <img src={google} alt="Google 로고 이미지" />
         Google {text}
       </button>
-      {userData ? userData.displayName : null}
     </>
   );
 }
