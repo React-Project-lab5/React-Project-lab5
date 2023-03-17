a26fe452f2b46afb5b2a9f5a7ba673511a1f13c4
hyebeen byun <hyebeen2658@naver.com>
2023-03-17 23:31:12 +0900

    [style] 회원가입 UI 구현

---
+++ src/components/SignUP/SignUp.tsx
@@ -0,0 +1,64 @@
+import classes from './SignUp.module.scss';
+import { Input } from '../Input/Input';
+import { LogoIconandText } from '../LogoIconandText/LogoIconandText';
+import { InputSelector } from '../InputSelector/InputSelector';
+export default function SignUp() {
+  const inputProps = {
+    maxWidthValue: '300px',
+    heightValue: '45px',
+    isA11yHidden: true,
+  };
+
+  return (
+    <div className={classes.formContainer}>
+      <div className={classes.formWrapper}>
+        <LogoIconandText small={false} />
+        <form>
+          <Input
+            type={'email'}
+            labelText={'이메일'}
+            placeHolder={'이메일을 입력하세요.'}
+            {...inputProps}
+          />
+          <button type="button" className={classes.button}>
+            이메일 인증
+          </button>
+          <Input
+            type={'password'}
+            labelText={'비밀번호'}
+            placeHolder={'비밀번호를 입력하세요.'}
+            {...inputProps}
+          />
+          <Input
+            type={'password'}
+            labelText={'비밀번호'}
+            placeHolder={'비밀번호를 한번 더 입력하세요.'}
+            {...inputProps}
+          />
+          <button type="button" className={classes.button}>
+            중복 확인
+          </button>
+          <Input
+            type={'name'}
+            labelText={'이름'}
+            placeHolder={'이름을 입력하세요.'}
+            {...inputProps}
+          />
+          <InputSelector
+            maxWidthValue={300}
+            heightValue={50}
+            onChange={undefined}
+            className={classes.selector}
+          />
+
+          <button type="button" className={classes.button}>
+            회원가입
+          </button>
+        </form>
+        <p>
+          <span>로그인</span> 계정이 있으신가요?
+        </p>
+      </div>
+    </div>
+  );
+}
