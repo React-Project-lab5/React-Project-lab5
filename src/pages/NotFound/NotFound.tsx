bca890b94a8529fea9cec2633dd01503444d272b
hyebeen byun <hyebeen2658@naver.com>
2023-03-18 00:18:26 +0900

    [style] NotFount Page UI 구현

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,12 @@
+import classes from './NotFound.module.scss';
 export default function NotFound() {
   return (
     <>
-      <h1>404</h1>
+      <div className={classes.container}>
+        <span>🚫 404 Error 🚫</span>
+        <h1>404</h1>
+        <p>존재 하지 않는 페이지 입니다..</p>
+      </div>
     </>
   );
 }
