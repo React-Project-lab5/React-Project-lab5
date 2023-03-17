91012de841d7c9372bcee5601e5c6c4ba3f5f868
hyebeen byun <hyebeen2658@naver.com>
2023-03-18 00:32:54 +0900

    [style] className 수정

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,9 +3,9 @@ export default function NotFound() {
   return (
     <>
       <div className={classes.container}>
-        <span>🚫 404 Error 🚫</span>
-        <h1>404</h1>
-        <p>존재 하지 않는 페이지 입니다..</p>
+        <span className={classes.span}>🚫 404 Error 🚫</span>
+        <h1 className={classes.title}>404</h1>
+        <p className={classes.content}>존재 하지 않는 페이지 입니다..</p>
       </div>
     </>
   );
