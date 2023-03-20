7922ae8f0df52991f09b867730cd126430e49fb2
hyebeen byun <hyebeen2658@naver.com>
2023-03-21 00:55:58 +0900

    [chore] AuthContextProvider 로 App감싸주기

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import ReactDOM from 'react-dom/client';
 import App from './App';
-import '@/styles/common/index.scss';
 import { RecoilRoot } from 'recoil';
+import '@/styles/common/index.scss';
+import ReactDOM from 'react-dom/client';
+import { AuthContextProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <>
     <RecoilRoot>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </RecoilRoot>
   </>
 );
