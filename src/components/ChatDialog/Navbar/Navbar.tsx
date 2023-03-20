390b2807fda527eef3f490e74c9946a9860e8fea
hyebeen byun <hyebeen2658@naver.com>
2023-03-21 01:23:19 +0900

    [feat] 로그인해서 받아온 데이터를 채팅 프로필에 연결

--- src/components/ChatDialog/Navbar/Navbar.tsx
+++ src/components/ChatDialog/Navbar/Navbar.tsx
@@ -1,5 +1,19 @@
 import classes from './Navbar.module.scss';
+import { signOut } from '@firebase/auth';
+import { auth } from '@/firebase/auth';
+import { useContext } from 'react';
+import { AuthContext } from '@/context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 export function Navbar() {
+  const navigation = useNavigate();
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSignOut = () => {
+    signOut(auth);
+
+    navigation('/');
+  };
+
   return (
     <div className={classes.navbar}>
       <p className={classes.logo}>슬기로운 N밥생활</p>
@@ -8,8 +22,10 @@ export function Navbar() {
           src="https://avatars.githubusercontent.com/u/104710243?v=4"
           alt="사용자"
         />
-        <p>변혜빈</p>
-        <button type="button">로그아웃</button>
+        <p>{currentUser.displayName}</p>
+        <button type="button" onClick={handleSignOut}>
+          로그아웃
+        </button>
       </div>
     </div>
   );
