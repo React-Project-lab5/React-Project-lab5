58fe573f88d7c4315bd4ebf63f594c12dd474b99
seoohyeon <fltk0825@naver.com>
2023-03-21 01:59:14 +0900

    [feat] Card - timeStamp 내림차순으로 정렬

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Banner, ShowMeetings } from '@/components/index';
 import { SearchFrom } from '@/components/Input/SearchForm';
 import classes from './MainPage.module.scss';
 import { db } from '@/firebase/firestore/index';
-import { collection, getDocs, addDoc } from '@firebase/firestore';
+import {
+  addDoc,
+  serverTimestamp,
+  collection,
+  query,
+  getDocs,
+  orderBy,
+} from '@firebase/firestore';
 import { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { cardDataState } from './../../states/cardDataState';
@@ -14,31 +22,30 @@ export default function MainPage() {
 
   const cardData = useRecoilValue(cardDataState);
 
-  // db의 users 컬렉션을 가져옴
-  const usersCollectionRef = collection(db, 'makeMeetings');
-
-  // 이따가 users 추가하고 삭제하는거 진행을 도와줄 state
   const [users, setUsers] = useState([]);
 
-  // 비동기로 데이터 받을준비
-  const getUsers = async () => {
-    // getDocs로 컬렉션안에 데이터 가져오기
-    const data = await getDocs(usersCollectionRef);
+  const usersCollectionRef = query(
+    collection(db, 'makeMeetings'),
+    orderBy('timestamp', 'desc')
+  );
 
-    // users에 data안의 자료 추가. 객체에 id 덮어씌우는거
-    setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  const getUsers = async () => {
+    await getDocs(usersCollectionRef).then((data) => {
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
   };
+
   useEffect(() => {
     getUsers();
   }, [cardData]);
 
   const createUsers = async () => {
-    // addDoc을 이용해서 내가 원하는 collection에 내가 원하는 key로 값을 추가한다.
-    await addDoc(usersCollectionRef, {
+    await addDoc(collection(db, 'makeMeetings'), {
       title: title,
       address: address,
       detail: detail,
       cardData: cardData,
+      timestamp: serverTimestamp(),
     });
   };
 
