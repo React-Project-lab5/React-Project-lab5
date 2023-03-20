477b0b299c2ae546bf45a74c74faeeb30abbb7cd
DahamWeon <onedaham@naver.com>
2023-03-20 17:31:01 +0900

    [style] Home UI 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,41 @@
+import classes from './Home.module.scss';
+import {
+  LogoIconandText,
+  ButtonGoogle,
+  ButtonKakao,
+  Button,
+} from '@/components/index';
+
 export default function Home() {
   return (
     <>
-      <h1>홈페이지임</h1>
+      <div className={classes.contents}>
+        <LogoIconandText small={false} />
+        <h2 className={classes.startText}>지금 시작하세요!</h2>
+        <ButtonGoogle text="회원가입" widthValue={'330px'} />
+        <ButtonKakao text="회원가입" widthValue={'330px'} />
+        <span className={classes.startText}>또는</span>
+        <Button
+          widthValue={'330px'}
+          colorValue={'#634718'}
+          text="이메일 회원가입"
+          className={classes.joinButton}
+        />
+        <span className={classes.startText}>회원이신가요?</span>
+        <Button
+          widthValue={'330px'}
+          colorValue={'#634718'}
+          text="로그인"
+          className={classes.loginButton}
+        />
+        <Button
+          widthValue={'330px'}
+          heightValue={'93px'}
+          colorValue={'#634718'}
+          text="게스트로 입장"
+          className={classes.guestButton}
+        />
+      </div>
     </>
   );
 }
