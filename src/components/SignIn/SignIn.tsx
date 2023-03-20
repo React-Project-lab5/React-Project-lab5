307e4f30ce1d835084473674419ef6ba76bc0709
hyebeen byun <hyebeen2658@naver.com>
2023-03-21 00:54:15 +0900

    [feat] 로그인 기능 구현

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -2,6 +2,10 @@ import classes from './SignIn.module.scss';
 import { Input } from '../Input/Input';
 import { LogoIconandText } from '../LogoIconandText/LogoIconandText';
 import { Button, ButtonGoogle, ButtonKakao } from '../Button';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from '@firebase/auth';
+import { auth } from '@/firebase/auth';
 export default function SignIn() {
   const inputProps = {
     maxWidthValue: '30',
@@ -9,20 +13,28 @@ export default function SignIn() {
     isA11yHidden: true,
   };
 
-  const buttonProps = {
-    backgroundColor: 'orange',
-    isSmall: false,
-    widthValue: '310',
-    heightValue: '50',
-    colorValue: 'white',
-    text: '로그인',
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert('성공');
+      navigate('/mainPage');
+    } catch (err) {
+      setErr(true);
+    }
   };
 
   return (
     <div className={classes.formContainer}>
       <div className={classes.formWrapper}>
         <LogoIconandText small={false} />
-        <form>
+        <form onSubmit={handleSubmit}>
           <Input
             type={'email'}
             placeHolder={'이메일을 입력하세요.'}
@@ -35,12 +47,24 @@ export default function SignIn() {
             placeHolder={'비밀번호를 입력하세요.'}
             {...inputProps}
           />
-          <Button {...buttonProps} />
+          <Button
+            backgroundColor={'orange'}
+            isSmall={false}
+            widthValue={'310'}
+            heightValue={'50'}
+            colorValue={'white'}
+            text={'로그인'}
+            type={'submit'}
+          />
+          {err && (
+            <span className={classes.error}>🚫 다시 입력해 주세요 🚫</span>
+          )}
         </form>
         <ButtonGoogle text="로그인" />
         <ButtonKakao text="로그인" />
         <p>
-          로그인 계정이 없으신가요?<span> 회원가입</span>
+          로그인 계정이 없으신가요?
+          <span> 회원가입</span>
         </p>
       </div>
     </div>
