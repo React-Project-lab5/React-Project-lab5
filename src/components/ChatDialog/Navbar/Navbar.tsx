c379e90061ed2b6391a62496b2f77f47fdd5806f
seoohyeon <fltk0825@naver.com>
2023-03-23 01:34:45 +0900

    [feat] fireStore에서 채팅 이미지 데이터 받아오기

--- src/components/ChatDialog/Navbar/Navbar.tsx
+++ src/components/ChatDialog/Navbar/Navbar.tsx
@@ -1,31 +1,58 @@
 import classes from './Navbar.module.scss';
 import { signOut } from '@firebase/auth';
 import { auth } from '@/firebase/auth';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '@/context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+
+import { db } from '@/firebase/firestore/index';
+import { collection, getDocs, query, where } from '@firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 export function Navbar() {
   const navigation = useNavigate();
   const { currentUser } = useContext(AuthContext);
+
+  const [cards, setCards] = useState([]);
 
   const handleSignOut = () => {
     signOut(auth);
     navigation('/');
   };
+  console.log('이거임', localStorage.getItem('navUid'));
 
-  return (
-    <div className={classes.navbar}>
+  const usersCollectionRef = query(
+    collection(db, 'users'),
+    where(
+      firebase.firestore.FieldPath.documentId(),
+      '==',
+      localStorage.getItem('navUid')
+    )
+  );
+
+  const getUsers = async () => {
+    await getDocs(usersCollectionRef).then((data) => {
+      setCards(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const showUserImage = cards.map((value, index: number) => (
+    <div key={index} className={classes.navbar}>
       <p className={classes.logo}>슬기로운 N밥생활</p>
       <div className={classes.user}>
-        <img
-          src="https://avatars.githubusercontent.com/u/104710243?v=4"
-          alt="사용자"
-        />
+        <img src={value.imageUrl} alt="사용자" />
         <p>{currentUser.displayName}</p>
         <button type="button" onClick={handleSignOut}>
           로그아웃
         </button>
       </div>
     </div>
-  );
+  ));
+
+  return <div>{showUserImage}</div>;
 }
