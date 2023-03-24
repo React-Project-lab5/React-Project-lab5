865359951464cc20e5d97cc5b59062f9e3be4ace
hyebeen byun <hyebeen2658@naver.com>
2023-03-24 17:06:11 +0900

    [feat] 채팅 이미지 업로드 기능 구현

--- src/components/ChatDialog/SendMessage/SendMessage.tsx
+++ src/components/ChatDialog/SendMessage/SendMessage.tsx
@@ -1,17 +1,25 @@
-import classes from './SendMessage.module.scss';
-import Img from '/public/assets/chatImagePlaceholder.svg';
-import { BsFillSendFill } from 'react-icons/bs';
-import { useContext, useState } from 'react';
-import { collection, addDoc, serverTimestamp } from '@firebase/firestore';
-import { AuthContext } from '@/context/AuthContext';
+import {
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+  UploadTaskSnapshot,
+} from '@firebase/storage';
 import { db } from '@/firebase/app';
+import { storage } from '@/firebase/storage';
+import { BsFillSendFill } from 'react-icons/bs';
+import classes from './SendMessage.module.scss';
+import { AuthContext } from '@/context/AuthContext';
+import { FormEvent, useContext, useState } from 'react';
+import Img from '/public/assets/chatImagePlaceholder.svg';
+import { collection, addDoc, serverTimestamp } from '@firebase/firestore';
 
 export function SendMessage() {
   const { currentUser } = useContext(AuthContext);
 
   const [input, setInput] = useState('');
+  const [img, setImg] = useState(null);
 
-  const sendMessage = async (e) => {
+  const handleSendMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (input === '') {
       alert('메시지를 입력하세요');
@@ -19,18 +27,50 @@ export function SendMessage() {
     }
     const { uid, displayName } = currentUser;
 
-    await addDoc(collection(db, 'messages'), {
-      text: input,
-      name: displayName,
-      uid,
-      createdAt: serverTimestamp(),
-    });
+    if (img) {
+      const uniqueId = img.name;
+      const storageRef = ref(storage, uniqueId);
+      const uploadTask = uploadBytesResumable(storageRef, img);
+
+      uploadTask.on(
+        'state_changed',
+        (snapshot: UploadTaskSnapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await addDoc(collection(db, 'messages'), {
+              text: input,
+              name: displayName,
+              uid,
+              createdAt: serverTimestamp(),
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } else {
+      await addDoc(collection(db, 'messages'), {
+        text: input,
+        name: displayName,
+        uid,
+        createdAt: serverTimestamp(),
+      });
+    }
+
     setInput('');
+    setImg(null);
   };
 
   return (
-    <form className={classes.input} onSubmit={sendMessage}>
+    <form className={classes.input} onSubmit={handleSendMessage}>
       <div>
+        <img src={img} alt="" />
         <label htmlFor="message" className="a11yHidden">
           메세지 입력
         </label>
@@ -44,7 +84,13 @@ export function SendMessage() {
         />
       </div>
       <div className={classes.send}>
-        <input type="file" style={{ display: 'none' }} id="file" name="file" />
+        <input
+          type="file"
+          style={{ display: 'none' }}
+          id="file"
+          name="file"
+          onChange={(e) => setImg(e.target.files[0])}
+        />
         <label htmlFor="file">
           <img src={Img} alt="이미지 업로드" />
         </label>
