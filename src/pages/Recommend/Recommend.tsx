ee501048b495e16dda051046a9c1c7b97c6dbe7d
hyebeen byun <hyebeen2658@naver.com>
2023-03-27 00:50:49 +0900

    [feat] 추천 페이지에 페이지 네이션 기능 추가

--- src/pages/Recommend/Recommend.tsx
+++ src/pages/Recommend/Recommend.tsx
@@ -1,41 +1,35 @@
 import axios from 'axios';
-import { Card } from '@/components';
 import { useEffect, useState } from 'react';
-import classes from './Recommend.module.scss';
+import { FoodList } from '@/components/FoodList/FoodList';
 import { useDocumentTitle } from '@/hooks/useDocumentTitle';
-import { useFetch } from '@/hooks/useFetch';
-
+import { Pagination } from '@/components/Pagination/Pagination';
+import { Banner } from '@/components';
 export default function Recommend() {
   useDocumentTitle('슬기로운 N밥 생활 | 추천');
 
-  const [foods, setFoods] = useState([]);
-  const [page, setPage] = useState(1);
-  const [perPage, setPerPage] = useState(10);
+  const [postsPerPage] = useState(12);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  // function useFoods({page = 1}) {
-  //   return useFetch(
-  //     `https://api.odcloud.kr/api/15097008/v1/uddi:1e5a6f2e-3f79-49bd-819b-d17541e6df78?page=${page}&perPage=${perPage}&serviceKey=${
-  //       import.meta.env.VITE_SERVICE_KEY
-  //     }`
-  //   );
-  // }
-
-  const url = `https://api.odcloud.kr/api/15097008/v1/uddi:1e5a6f2e-3f79-49bd-819b-d17541e6df78?page=${page}&perPage=${perPage}&serviceKey=${
+  const url = `https://api.odcloud.kr/api/15097008/v1/uddi:1e5a6f2e-3f79-49bd-819b-d17541e6df78?page=5&perPage=60&serviceKey=${
     import.meta.env.VITE_SERVICE_KEY
   }`;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(url);
-        console.log(response.data);
-        setFoods(response.data.data);
+        setLoading(true);
+        const { data } = await axios.get(url);
+        console.log(data);
+        setPosts(data.data);
+        setLoading(false);
       } catch (error) {
         if (error.response) {
-          console.log(error.response.data);
-          console.log(error.response.status);
+          console.log(error.data);
+          console.log(error.status);
         } else {
-          console.log(error.message);
+          console.error(error.message);
         }
       }
     };
@@ -43,25 +37,24 @@ export default function Recommend() {
     fetchData();
   }, [url]);
 
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   return (
     <>
-      <h1>추천</h1>
-      <div className={classes.store}>
-        {foods.map((food, index) => (
-          <Card key={index}>
-            <div className={classes.storeBox}>
-              <p className={classes.LocalName}>{food['지역명']}</p>
-              <p className={classes.storeName}>{food['식당명']}</p>
-              <img
-                src={food['음식이미지(URL)']}
-                alt={'음식이미지'}
-                width={150}
-                height={150}
-              />
-            </div>
-          </Card>
-        ))}
-      </div>
+      <Banner />
+      <h1 className="a11yHidden"> 서울 음식점 추천</h1>
+      <FoodList posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </>
   );
 }
