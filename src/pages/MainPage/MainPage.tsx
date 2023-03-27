0f79d58e048ef19f8b0a38d0cbb634a1e12f3185
hyebeen byun <hyebeen2658@naver.com>
2023-03-27 22:44:54 +0900

    [feat] - 페이지네이션 기능 추가 - scrollToTop 버튼 추가

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -10,20 +10,23 @@ import {
   orderBy,
   doc,
 } from '@firebase/firestore';
-import { useEffect } from 'react';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { cardDataState } from '@/@recoil/cardDataState';
-import { mapState } from '@/@recoil/mapState';
-import { titleMainState } from '@/@recoil/titleMainState';
-import { addressMainState } from '@/@recoil/addressMainState';
-import { detailMainState } from '@/@recoil/detailMainState';
-import { usersState } from '@/@recoil/usersState';
-import { auth } from '@/firebase/auth';
-import { useDocumentTitle } from '@/hooks/useDocumentTitle';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import { auth } from '@/firebase/auth';
 import { Banner } from '@/components/index';
-import { SearchFrom } from '@/components/Input/SearchForm';
+import { mapState } from '@/@recoil/mapState';
+import { IoIosArrowUp } from 'react-icons/io';
 import { ShowMeetings } from '@/components/index';
+import { usersState } from '@/@recoil/usersState';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { cardDataState } from '@/@recoil/cardDataState';
+import classes from '../Recommend/Recommend.module.scss';
+import { titleMainState } from '@/@recoil/titleMainState';
+import { SearchFrom } from '@/components/Input/SearchForm';
+import { detailMainState } from '@/@recoil/detailMainState';
+import { addressMainState } from '@/@recoil/addressMainState';
+import { useDocumentTitle } from '@/hooks/useDocumentTitle';
+import { Pagination } from '@/components/Pagination/Pagination';
 
 export default function MainPage() {
   useDocumentTitle('슬기로운 N밥생활 | 모임');
@@ -33,9 +36,14 @@ export default function MainPage() {
   const detail = useRecoilValue(detailMainState);
   const cardData = useRecoilValue(cardDataState);
   const mapData = useRecoilValue(mapState);
-  const setUsers = useSetRecoilState(usersState);
+  // const setUsers = useSetRecoilState(usersState);
   const [name, setName] = useState('');
   const [userImg, setUserImg] = useState('');
+
+  const [users, setUsers] = useRecoilState(usersState);
+  const [currentPost, setCurrentPost] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+  const postsPerPage = 6; // 페이지당 게시물 수
 
   useEffect(() => {
     const unsub = auth.onAuthStateChanged((user) => {
@@ -89,11 +97,44 @@ export default function MainPage() {
     getUsers: getUsers,
   };
 
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  // Get current posts
+  useEffect(() => {
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = users.slice(indexOfFirstPost, indexOfLastPost);
+    setCurrentPost(currentPosts);
+  }, [currentPage, users, postsPerPage]);
+
+  // Change page
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
   return (
     <>
       <Banner />
       <SearchFrom {...searchFormProps} />
       <ShowMeetings />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={users.length}
+        paginate={paginate}
+      />
+      <div className={classes.container}>
+        <button
+          type="button"
+          className={classes.button}
+          onClick={handleScrollToTop}
+          tabIndex={0}
+        >
+          <IoIosArrowUp size={30} />
+        </button>
+      </div>
     </>
   );
 }
--- src/pages/Recommend/Recommend.tsx
+++ src/pages/Recommend/Recommend.tsx
@@ -1,18 +1,24 @@
 import axios from 'axios';
+import { Banner } from '@/components';
+import { useRecoilState } from 'recoil';
 import { useEffect, useState } from 'react';
+import classes from './Recommend.module.scss';
+import { IoIosArrowUp } from 'react-icons/io';
 import { FoodList } from '@/components/FoodList/FoodList';
 import { useDocumentTitle } from '@/hooks/useDocumentTitle';
 import { Pagination } from '@/components/Pagination/Pagination';
-import { Banner } from '@/components';
+import { currentPageState, postsState } from '@/@recoil/pageState';
+
 export default function Recommend() {
   useDocumentTitle('슬기로운 N밥 생활 | 추천');
 
-  const [postsPerPage] = useState(12);
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 28;
 
-  const url = `https://api.odcloud.kr/api/15097008/v1/uddi:1e5a6f2e-3f79-49bd-819b-d17541e6df78?page=5&perPage=60&serviceKey=${
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useRecoilState(currentPageState);
+  const [posts, setPosts] = useRecoilState(postsState);
+
+  const url = `https://api.odcloud.kr/api/15097008/v1/uddi:1e5a6f2e-3f79-49bd-819b-d17541e6df78?page=4&perPage=112&serviceKey=${
     import.meta.env.VITE_SERVICE_KEY
   }`;
 
@@ -21,7 +27,6 @@ export default function Recommend() {
       try {
         setLoading(true);
         const { data } = await axios.get(url);
-        console.log(data);
         setPosts(data.data);
         setLoading(false);
       } catch (error) {
@@ -35,7 +40,11 @@ export default function Recommend() {
     };
 
     fetchData();
-  }, [url]);
+  }, [setPosts, url]);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   // Get current posts
   const indexOfLastPost = currentPage * postsPerPage;
@@ -48,13 +57,24 @@ export default function Recommend() {
   return (
     <>
       <Banner />
-      <h1 className="a11yHidden"> 서울 음식점 추천</h1>
+
+      <h1 className={classes.title}> 서울 맛집 추천</h1>
       <FoodList posts={currentPosts} loading={loading} />
       <Pagination
         postsPerPage={postsPerPage}
         totalPosts={posts.length}
         paginate={paginate}
       />
+      <div className={classes.container}>
+        <button
+          type="button"
+          className={classes.button}
+          onClick={handleScrollToTop}
+          tabIndex={0}
+        >
+          <IoIosArrowUp size={30} />
+        </button>
+      </div>
     </>
   );
 }
