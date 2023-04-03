3f239d7bb65a67301cd0c5d8e1cbc0f4715c7eff
seoohyeon <fltk0825@naver.com>
2023-04-03 20:17:04 +0900

    merge

--- src/components/FoodList/FoodList.tsx
+++ src/components/FoodList/FoodList.tsx
@@ -11,9 +11,9 @@ interface Food {
 
 interface Props {
   posts: Food[];
-  loading: boolean;
 }
 
+<<<<<<< Updated upstream
 export function FoodList({ posts, loading }: Props) {
   const [showCards, setShowCards] = useState<boolean>(false);
 
@@ -33,9 +33,14 @@ export function FoodList({ posts, loading }: Props) {
       </div>
     );
   }
+=======
+const FoodItems = lazyMinLoadTime(() => import('./FoodItems'), 5000);
+>>>>>>> Stashed changes
 
+export function FoodList({ posts }: Props) {
   return (
     <div className={classes.store}>
+<<<<<<< Updated upstream
       {showCards &&
         posts.map((food: Food, index: number) => (
           <Card key={index} className={classes.card}>
@@ -51,6 +56,11 @@ export function FoodList({ posts, loading }: Props) {
             </div>
           </Card>
         ))}
+=======
+      <React.Suspense fallback={<div>로딩 중...</div>}>
+        <FoodItems posts={posts} />
+      </React.Suspense>
+>>>>>>> Stashed changes
     </div>
   );
 }
