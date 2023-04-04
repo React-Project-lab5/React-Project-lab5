17df6f5dfc8061427740ef3e8703ae9a6aeb27e6
hyebeen byun <hyebeen2658@naver.com>
2023-04-04 22:29:45 +0900

    [feat] 추천 검색필터링 구현

--- src/pages/Recommend/Recommend.tsx
+++ src/pages/Recommend/Recommend.tsx
@@ -1,22 +1,27 @@
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 import axios from 'axios';
-import { Banner } from '@/components';
+import { Banner, Input } from '@/components';
 import { useEffect, useState } from 'react';
 import classes from './Recommend.module.scss';
 import { FoodList } from '@/components/FoodList/FoodList';
 import { useDocumentTitle } from '@/hooks/useDocumentTitle';
 import { Pagination } from '@/components/Pagination/Pagination';
 import { ScrollButton } from '@/components/Button/ScrollButton/ScrollButton';
-
+import search from '/public/assets/search.svg';
+import { useRecoilState } from 'recoil';
+import { searchTermState } from '@/@recoil/searchTermState';
+import { loadingState } from '@/@recoil/loadingState';
 export default function Recommend() {
   useDocumentTitle('슬기로운 N밥 생활 | 추천');
 
-  const postsPerPage = 20;
+  const postsPerPage = 24;
 
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useRecoilState(loadingState);
+  const [searchTerm, setSearchTerm] = useRecoilState(searchTermState);
 
-  const API_URL = `https://api.odcloud.kr/api/15097008/v1/uddi:1e5a6f2e-3f79-49bd-819b-d17541e6df78?page=3&perPage=80&serviceKey=${
+  const API_URL = `https://api.odcloud.kr/api/15097008/v1/uddi:1e5a6f2e-3f79-49bd-819b-d17541e6df78?page=3&perPage=96&serviceKey=${
     import.meta.env.VITE_SERVICE_KEY
   }`;
 
@@ -25,6 +30,7 @@ export default function Recommend() {
       setLoading(true);
       try {
         const { data } = await axios.get(API_URL);
+
         setPosts(data.data);
       } catch (error) {
         console.error(error);
@@ -33,20 +39,58 @@ export default function Recommend() {
     };
 
     fetchData();
-  }, [API_URL, currentPage]);
+  }, [API_URL, currentPage, setLoading]);
 
-  // Get current posts
+  // 현재 게시물 가져오기
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
-  // Change page
+  // 페이지 변경
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  // 검색어를 상태에 업데이트
+  const handlerSearchTerm = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <>
       <Banner />
       <h1 className={classes.title}> 서울 맛집 추천</h1>
+      <div className={classes['InputContainer']}>
+        <div className={classes['mainInput']}>
+          <form
+            className={classes['formInput']}
+            role="search"
+            onSubmit={(e) => {
+              e.preventDefault();
+              setCurrentPage(1);
+            }}
+          >
+            <div className={classes['inputSearchButton']}>
+              <Input
+                maxWidthValue={'35rem'}
+                heightValue={'75px'}
+                labelText="검색창"
+                placeHolder="제목을 검색하세요."
+                isA11yHidden
+                className={classes.searchFormInput}
+                value={searchTerm} // 검색어 상태를 value 속성에 전달
+                onChange={handlerSearchTerm}
+              />
+              <button
+                className={classes['searchButton']}
+                type="submit"
+                aria-label="검색 버튼"
+                tabIndex={0}
+              >
+                <img src={search} alt="검색 버튼" tabIndex={0} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
       <FoodList posts={currentPosts} loading={loading} />
       <Pagination
         postsPerPage={postsPerPage}
