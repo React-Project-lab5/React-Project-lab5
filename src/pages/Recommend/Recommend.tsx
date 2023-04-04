2fb9887fdda80feab613a08a85981da53d9d04f1
hyebeen byun <hyebeen2658@naver.com>
2023-04-05 00:14:05 +0900

    [feat] 페이지네이션 수정

--- src/pages/Recommend/Recommend.tsx
+++ src/pages/Recommend/Recommend.tsx
@@ -1,27 +1,28 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 import axios from 'axios';
-import { Banner, Input } from '@/components';
-import { useEffect, useState } from 'react';
-import classes from './Recommend.module.scss';
-import { FoodList } from '@/components/FoodList/FoodList';
-import { useDocumentTitle } from '@/hooks/useDocumentTitle';
-import { Pagination } from '@/components/Pagination/Pagination';
-import { ScrollButton } from '@/components/Button/ScrollButton/ScrollButton';
-import search from '/public/assets/search.svg';
 import { useRecoilState } from 'recoil';
-import { searchTermState } from '@/@recoil/searchTermState';
+import ReactPaginate from 'react-paginate';
+import { useEffect, useState } from 'react';
+import { Banner, Input } from '@/components';
+import classes from './Recommend.module.scss';
+import search from '/public/assets/search.svg';
 import { loadingState } from '@/@recoil/loadingState';
+import { FoodList } from '@/components/FoodList/FoodList';
+import { searchTermState } from '@/@recoil/searchTermState';
+import { useDocumentTitle } from '@/hooks/useDocumentTitle';
+import { ScrollButton } from '@/components/Button/ScrollButton/ScrollButton';
+
 export default function Recommend() {
   useDocumentTitle('슬기로운 N밥 생활 | 추천');
 
-  const postsPerPage = 24;
+  const postsPerPage = 20;
 
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useRecoilState(loadingState);
   const [searchTerm, setSearchTerm] = useRecoilState(searchTermState);
 
-  const API_URL = `https://api.odcloud.kr/api/15097008/v1/uddi:1e5a6f2e-3f79-49bd-819b-d17541e6df78?page=3&perPage=96&serviceKey=${
+  const API_URL = `https://api.odcloud.kr/api/15097008/v1/uddi:1e5a6f2e-3f79-49bd-819b-d17541e6df78?page=1&perPage=160&serviceKey=${
     import.meta.env.VITE_SERVICE_KEY
   }`;
 
@@ -42,17 +43,20 @@ export default function Recommend() {
   }, [API_URL, currentPage, setLoading]);
 
   // 현재 게시물 가져오기
-  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfLastPost = (currentPage + 1) * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   // 페이지 변경
-  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  const handlePageClick = ({ selected }) => setCurrentPage(selected);
 
   // 검색어를 상태에 업데이트
   const handlerSearchTerm = (e) => {
     setSearchTerm(e.target.value);
   };
+
+  // 총 페이지 수
+  const totalPageNum = Math.ceil(posts.length / postsPerPage);
 
   return (
     <>
@@ -92,11 +96,18 @@ export default function Recommend() {
         </div>
       </div>
       <FoodList posts={currentPosts} loading={loading} />
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
-        paginate={paginate}
+      <ReactPaginate
+        previousLabel={'<'}
+        nextLabel={'>'}
+        breakLabel={'...'}
+        pageCount={totalPageNum}
+        marginPagesDisplayed={2} //현재 페이지에서 양쪽으로 표시될 페이지 수
+        pageRangeDisplayed={3} //페이지 버튼 그룹에서 표시될 페이지 수
+        onPageChange={handlePageClick}
+        containerClassName={classes.pagination} //페이지 버튼들을 감싸는 div에 적용될 클래스 이름
+        activeClassName={classes.active} //현재 페이지에 해당하는 버튼에 적용될 클래스 이름
       />
+
       <ScrollButton />
     </>
   );
