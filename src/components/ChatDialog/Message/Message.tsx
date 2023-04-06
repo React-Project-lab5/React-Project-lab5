ba65dafcc3016c15429830fff7096c1ddb872f81
hyebeen byun <hyebeen2658@naver.com>
2023-04-06 22:23:45 +0900

    [feat] 이미지 올릴시 그 이미지로 프로필이 마뀌는 문제 해결

--- src/components/ChatDialog/Message/Message.tsx
+++ src/components/ChatDialog/Message/Message.tsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'react';
-import { useRecoilValue } from 'recoil';
+import { db } from '@/firebase/app';
 import classNames from 'classnames';
+import { auth } from '@/firebase/auth';
+import { useRecoilValue } from 'recoil';
+import { useState, useEffect } from 'react';
 import classes from './Message.module.scss';
 import { authImagState } from '@/@recoil/authImgState';
 import defaultAvatar from '/public/assets/defaultAvatars.svg';
-import { collection, doc, getDoc } from '@firebase/firestore';
-import { db } from '@/firebase/app';
-import { auth } from '@/firebase/auth';
+import { collection, doc, getDoc, onSnapshot } from '@firebase/firestore';
 
 interface MessageProps {
   message: {
@@ -18,12 +18,22 @@ interface MessageProps {
 
 export function Message({ message }: MessageProps) {
   const [photoURL, setPhotoURL] = useState<string>(defaultAvatar);
+
+  //authImagState는 현재 로그인한 사용자의 프로필 이미지
   const imageUrl = useRecoilValue(authImagState);
 
   useEffect(() => {
+    if (!message.uid) {
+      return;
+    }
+
+    const userRef = doc(collection(db, 'users'), message.uid);
+
+    //해당 데이터를 필요로 하지 않는 경우, unsubscribe()를 호출하여 해당 데이터의 수신을 중단
+    let unsubscribe: (() => void) | undefined;
+
     async function getUserDoc() {
-      const docRef = doc(collection(db, 'users'), message.uid);
-      const docSnap = await getDoc(docRef);
+      const docSnap = await getDoc(userRef);
 
       if (docSnap.exists()) {
         const userData = docSnap.data();
@@ -31,17 +41,32 @@ export function Message({ message }: MessageProps) {
       }
     }
 
-    getUserDoc();
+    getUserDoc().then(() => {
+      unsubscribe = onSnapshot(userRef, (docSnap) => {
+        const userData = docSnap.data();
+
+        if (userData?.photoURL) {
+          setPhotoURL(userData.photoURL);
+        }
+      });
+    });
+
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
   }, [message.uid]);
 
+  // 현재 메시지의 프로필 이미지 URL을 반환
   function getImageUrl() {
-    if (message.photoURL) {
-      return message.photoURL;
+    if (imageUrl) {
+      return isCurrentUser() ? imageUrl : photoURL;
     }
 
-    return isCurrentUser() ? imageUrl : photoURL;
+    return photoURL;
   }
-
+  // 현재 사용자가 메시지 작성자인지 확인
   function isCurrentUser() {
     return auth.currentUser?.uid === message.uid;
   }
