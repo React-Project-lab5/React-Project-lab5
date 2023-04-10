a97c19641fed3c9f5e1c3008924d38668017ab10
hyebeen byun <hyebeen2658@naver.com>
2023-04-10 21:43:15 +0900

    [chore] doc type 지정

--- src/components/Input/SearchForm.tsx
+++ src/components/Input/SearchForm.tsx
@@ -1,4 +1,11 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  QueryDocumentSnapshot,
+} from '@firebase/firestore';
 import { Input } from './Input';
 import { Button } from '../Button';
 import { Card } from '@/@recoil/usersState';
@@ -13,14 +20,13 @@ import { addressState } from '@/@recoil/addressState';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 import { SetStateAction, useEffect, useState } from 'react';
 import { InputSelector } from '../InputSelector/InputSelector';
-import { collection, query, where, getDocs } from '@firebase/firestore';
 
 export function SearchFrom({ createUsers, getUsers }: SearchFormProps) {
+  const movePage = useNavigate();
   const [searchTitle, setSearchTitle] = useState('');
   const setUsers = useSetRecoilState(usersState);
   const [address, setAddress] = useRecoilState(addressState);
-  const movePage = useNavigate();
-  const [arrayTitle, setArraytitle] = useState([]);
+  const [arrayTitle, setArrayTitle] = useState([]);
   const [inputFlag, setInputFlag] = useState(false);
 
   useEffect(() => {
@@ -57,10 +63,12 @@ export function SearchFrom({ createUsers, getUsers }: SearchFormProps) {
 
     try {
       const querySnapshot = await getDocs(usersCollectionRef);
-      const usersData = querySnapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      })) as Card[];
+      const usersData = querySnapshot.docs.map(
+        (doc: QueryDocumentSnapshot<Card>) => ({
+          ...doc.data(),
+          id: doc.id,
+        })
+      ) as Card[];
       setUsers(
         usersData.sort((a, b) => {
           if (a.timestamp.seconds < b.timestamp.seconds) return 1;
@@ -79,7 +87,7 @@ export function SearchFrom({ createUsers, getUsers }: SearchFormProps) {
 
   const writeTitle = (e: { target: { value: SetStateAction<string> } }) => {
     setSearchTitle(e.target.value);
-    setArraytitle([...searchTitle]);
+    setArrayTitle([...searchTitle]);
     console.log(e.target.value);
 
     if (e.target.value === '') {
