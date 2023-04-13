bf55c72afdd39ea797ea956d6bc32c4ab54c97f9
seoohyeon <fltk0825@naver.com>
2023-04-13 18:28:51 +0900

    [a11y] 성능 개선 - 이미지 크기값 부여

--- src/components/SocialIconGroup/SocialIconGroup.tsx
+++ src/components/SocialIconGroup/SocialIconGroup.tsx
@@ -8,11 +8,11 @@ import Instargram from '../../../public/assets/instagram.svg';
 export function SocialIconGroup() {
   return (
     <div className={classes['socialIconGroupWrapper']}>
-      <img src={Blog} alt="네이버 블로그" />
-      <img src={Facebook} alt="페이스북" />
-      <img src={Instargram} alt="인스타그램" />
-      <img src={NaverPost} alt="네이버 포스트" />
-      <img src={Youtube} alt="유튜브" />
+      <img src={Blog} alt="네이버 블로그" width={30} height={30} />
+      <img src={Facebook} alt="페이스북" width={30} height={30} />
+      <img src={Instargram} alt="인스타그램" width={30} height={30} />
+      <img src={NaverPost} alt="네이버 포스트" width={30} height={30} />
+      <img src={Youtube} alt="유튜브" width={30} height={30} />
     </div>
   );
 }
